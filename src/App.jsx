c9be59c06a9560d567d204e94f66ded63abe83a9
Jacob Feng <jfeng4@ualberta.ca>
2023-03-28 18:27:29 -0600

    Remove sign in pages and references

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,6 @@ import './css/style.css';
 import AOS from 'aos';
 
 import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
 import ResetPassword from './pages/ResetPassword';
 
 function App() {
@@ -38,8 +36,6 @@ function App() {
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
         <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </>
